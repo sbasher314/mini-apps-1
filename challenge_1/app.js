1c43bc49de75cf4cff60f63e1ccf11ed3a9606c3
Steven Basher <sbasher314@gmail.com>
2021-03-23 22:34:35 -0400

    Initial setup of gameboard, reset button

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,21 @@
+window.addEventListener('load', event => {
+  let gameBoard = [[,,,],[,,,],[,,,]];
+  let turn = 0;
+  let players = {
+    0: 'Player 1',
+    1: 'Player 2',
+    tie: 'Tie'
+  }
 
+  let reset = () => {
+    gameBoard = [[,,,],[,,,],[,,,]];
+    turn = 0;
+    document.querySelector('.status').innerHTML = "Player 1's turn";
+  }
 
+  document.querySelector('.reset').addEventListener('click', event => {
+    console.log('Resetting board');
+    reset();
+  });
+
+});
