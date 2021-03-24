d1b1c8cda9d0f5640a26780e134bf27396868342
Steven Basher <sbasher314@gmail.com>
2021-03-23 23:08:57 -0400

    Complete tic-tac-toe Has solution highlighting and simple stylings

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', event => {
   let gameBoard = [[,,,],[,,,],[,,,]];
   let turn = 0;
+  let solution = [,,,];
   let players = {
     0: 'Player 1',
     1: 'Player 2',
@@ -10,6 +11,11 @@ window.addEventListener('load', event => {
   let reset = () => {
     gameBoard = [[,,,],[,,,],[,,,]];
     turn = 0;
+    let elements = [...document.getElementsByClassName('cell')];
+    elements.forEach(el => {
+      el.classList.remove('set', 'correct');
+      el.innerHTML = "";
+    });
     document.querySelector('.status').innerHTML = "Player 1's turn";
   }
 
@@ -20,6 +26,7 @@ window.addEventListener('load', event => {
         continue;
       }
       if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+        solution = [[i,0],[i,1],[i,2]];
         return winner;
       }
     }
@@ -32,6 +39,7 @@ window.addEventListener('load', event => {
         continue;
       }
       if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+        solution = [[0,i],[1,i],[2,i]];
         return winner;
       }
     }
@@ -41,12 +49,14 @@ window.addEventListener('load', event => {
     let winner = board[0][0];
     if (winner !== undefined) {
       if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+        solution = [[0,0],[1,1],[2,2]];
         return winner;
       }
     }
     winner = board[2][0];
     if (winner !== undefined) {
       if (board[2][0] === board[1][1] && board[1][1] === board[0][2]) {
+        solution = [[2,0],[1,1],[0,2]];
         return winner;
       }
     }
@@ -79,30 +89,37 @@ window.addEventListener('load', event => {
     return players[winner];
   }
 
+  let highlightSolution = () => {
+    for (let i = 0; i < solution.length; i++) {
+      let cell = document.querySelector(`[row="${solution[i][0]}"][col="${solution[i][1]}"]`);
+      cell.classList.add('correct');
+    }
+  }
+
   document.querySelector('.reset').addEventListener('click', event => {
     console.log('Resetting board');
     reset();
   });
 
   document.querySelector('.board').addEventListener('click', event => {
-    console.log(gameBoard);
     let row = event.target.getAttribute('row');
     let col = event.target.getAttribute('col');
     if (event.target.classList.contains('set') === false && gameBoard[row][col] === undefined) {
-      let player = turn++ % 2
+      let player = turn % 2
       gameBoard[row][col] = player;
       event.target.classList.add('set');
+      event.target.innerHTML = (player === 0) ? 'X' : 'O';
       let winner = checkSolution(gameBoard);
-      console.log(winner);
       let status = '';
-      if (winner === 'tie') {
+      if (winner === 'Tie') {
         status = 'No winner - Tie';
       } else if (winner !== undefined) {
         status = winner + " wins!";
+        highlightSolution();
         let elements = [...document.getElementsByClassName('cell')];
         elements.forEach(el => el.classList.add('set'));
       } else {
-        status = players[player] + "'s turn";
+        status = players[++turn % 2] + "'s turn";
       }
       document.querySelector('.status').innerHTML = status;
     } else {
